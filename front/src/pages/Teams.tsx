6f60ca5f76069ea3c82d6cd282d6b98ad0e86cc1
EnricOclock <enric.juanpro@gmail.com>
2025-11-05 00:26:59 +0100

    Implement Create and Delete Team features

--- front/src/pages/Teams.tsx
+++ front/src/pages/Teams.tsx
@@ -3,9 +3,14 @@ import React from 'react';
 import Modal from "../components/ui/modal";
 import { useState } from 'react';
 import OrbitCarousel from '@/components/ui/orbit-carousel';
-import { useGetAllTeamsQuery } from "@/store/api/teamApi";
+import { Trash } from "lucide-react";
+import { useCreateOneTeamMutation, useDeleteOneTeamMutation, useGetAllTeamsQuery, useRemovePokemonToTeamMutation } from "@/store/api/teamApi";
 import type { FetchBaseQueryError } from '@reduxjs/toolkit/query';
 import type { Team } from "@/store/api/teamApi";
+import { Button } from '@/components/ui/button';
+
+
+
 
 export interface TeamMemberCardProps {
   team: Team;
@@ -23,23 +28,67 @@ const TeamMemberCard: React.FC<TeamMemberCardProps> = ({ team, onClick }) => (
 const Teams: React.FC = () => {
 
   const [showTeamModal, setShowTeamModal] = useState(false);
+  const [showCreateTeamModal, setShowCreateTeamModal] = useState(false);
   const [selectedTeam, setSelectedTeam] = useState<Team>();
+  const [teamName, setTeamName] = useState('');
+  const [teamDescription, setTeamDescription] = useState('');
+
   const teamMembers = selectedTeam?.pokemons
 
-  const { data: teams, isLoading, isError, error} = useGetAllTeamsQuery(); 
+  const { data: teams, isLoading: teamsLoading, isError: teamsIsError, error: teamsError} = useGetAllTeamsQuery();
+  const [updateTeam, { isLoading: updateIsLoading, isError: updateIsError }] = useCreateOneTeamMutation();
+  const [deleteTeam, { isLoading: deleteIsLoading, isError: deleteIsError }] = useDeleteOneTeamMutation();
 
-  console.log('MEMBERS SELECTED: ', teamMembers)
+  const [createTeam, { isLoading, isError }] = useCreateOneTeamMutation();
 
-
-  if (isLoading) return <p>Chargement...</p>;
-  if (isError) return <p>Erreur : {(error as FetchBaseQueryError).status}</p>;
+  if (teamsLoading || isLoading || updateIsLoading || deleteIsLoading) return <p>Chargement...</p>;
+  if (teamsIsError || isError || updateIsError || deleteIsError) return <p>Erreur : {(teamsError as FetchBaseQueryError).status}</p>;
 
   function handleShowTeam (team: Team) {
     setSelectedTeam(team);
     setShowTeamModal(true);
   }
 
-  console.log('TEAM DATA :',  teams)
+  function handleShowCreateTeamForm () {
+    setShowCreateTeamModal(true);
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = {
+      'name': teamName,
+      'description' : teamDescription
+    };
+
+    try {
+        const response = await createTeam(formData).unwrap();
+        console.log('✅ Équipe créée :', response);
+
+        // Reset du form et fermeture de la modal
+        setShowCreateTeamModal(false);
+        setTeamName('');
+        setTeamDescription('');
+      } catch (err) {
+        console.error('❌ Erreur lors de la création de l’équipe :', err);
+      }
+  };
+
+  async function handleDeleteTeam (selectedTeamId?: string) {
+
+    if (!selectedTeamId) return;
+
+    const teamId = String(selectedTeamId);
+
+    console.log('TEAMID :', teamId)
+
+    try {
+      const response = await deleteTeam( teamId ).unwrap();
+      console.log('✅ Équipe supprimée :', response);
+      setShowTeamModal(false);
+    } catch (err) {
+      console.error('❌ Erreur lors de la suppression de l’équipe :', err);
+    }
+  }
 
   return (
     <section className="font-sans">
@@ -55,8 +104,8 @@ const Teams: React.FC = () => {
         </div>
 
         {/* Team Members Grid - Adjusted for 12 members */}
-        <div  className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-x-6 gap-y-12 p-4 border rounded-2xl shadow-2xl">
-          <ul className='flex flex-col gap-4'>
+        <div  className=" gap-6 p-4 border rounded-2xl shadow-2xl">
+          <ul className=''>
           {teams?.map((team) => (
             
             <li className='text-center items-center'>
@@ -66,13 +115,56 @@ const Teams: React.FC = () => {
           ))}
           </ul>
         </div>
+          <Button onClick={handleShowCreateTeamForm} className='w-full'>+</Button>
               <Modal
                 isOpen={showTeamModal}
                 onClose={() => setShowTeamModal(false)}
                 title={`${selectedTeam?.name}`}
               >
-                <OrbitCarousel teamMembers={teamMembers}/>
+                <h1 className='text-center'>{selectedTeam?.description}</h1>
+                
+                <Button onClick={() => handleDeleteTeam(selectedTeam?.id)}><Trash/></Button>
+                <OrbitCarousel teamMembers={teamMembers} />
           
+              </Modal>
+              <Modal
+                isOpen={showCreateTeamModal}
+                onClose={() => setShowCreateTeamModal(false)}
+                title={"Création d'une équipe"}
+              >
+                <div className=''>
+                  <form onSubmit={handleSubmit} className='space-y-4'>
+                    <div>
+                      <label>Saisissez le nom de l'équipe</label>
+                      <input 
+                        type="text"
+                        autoComplete='username'
+                        value={teamName}
+                        onChange={(e) => setTeamName(e.target.value)}
+                        required
+                        placeholder="Team Roquette"
+                        className="w-full pl-10 pr-4 py-3 rounded-lg border border-gray-300 focus:ring-2 focus:ring-orange-500 focus:border-transparent transition-all"
+                        >
+                      </input>
+                    </div>
+                    <div className='space-y-1'>
+                      <label >Redigez une courte déscription</label>
+                      <input 
+                        placeholder="Description"
+                        type="text"
+                        autoComplete='username'
+                        value={teamDescription}
+                        onChange={(e) => setTeamDescription(e.target.value)}
+                        required
+                        className="w-full pl-10 pr-4 py-3 rounded-lg border border-gray-300 focus:ring-2 focus:ring-orange-500 focus:border-transparent transition-all"
+                        >    
+                      </input>
+                    </div>
+                    <div>
+                      <Button>Créer</Button>
+                    </div>
+                  </form>
+                </div>
               </Modal>
       </div>
     </section>
